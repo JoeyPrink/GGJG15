0f1f91a9778b04865523f2910ca89e20343486c5
Johanna Pirker <joeyprink@gmail.com>
2015-01-25 15:00:29 +0100

    outro

--- GameAfter/Assets/EvilOrGood.cs
+++ GameAfter/Assets/EvilOrGood.cs
@@ -4,38 +4,65 @@ using System.Collections;
 public class EvilOrGood : MonoBehaviour
 {
 	
-	public Sprite sprite1; // good 
-	public Sprite sprite2; // evil
-
-	public bool lockSecondSprite = true;
-	private bool evil = false;
-	private SpriteRenderer spriteRenderer;
-	
-	void Start ()
-	{
-		spriteRenderer = GameObject.Find ("EvilOrGoodScreen").GetComponent<SpriteRenderer> ();
-		spriteRenderer.sprite = null; 
-	}
-	
-	void Update ()
-	{
-	}
-	
-	void changeSprites ()
-	{
-		if (evil) { 
-			spriteRenderer.sprite = sprite2;
-		} else {
-			spriteRenderer.sprite = sprite1; 
-		}
-	}
+		public Sprite sprite1; // good 
+		public Sprite sprite2; // evil
 
 	
-	void OnTriggerEnter2D (Collider2D col)
-	{
-		if (col.gameObject.tag == "Player") {
-			changeSprites();
+		public Sprite sprite3; // good 
+		public Sprite sprite4; // evil
+
+
+		public bool lockSecondSprite = true;
+		private bool evil = false;
+		private SpriteRenderer spriteRenderer;
+		private SpriteRenderer spriteRenderer2;
+		private bool end;
+		public int counter = 400;
+	
+		void Start ()
+		{
+				spriteRenderer = GameObject.Find ("EvilOrGoodScreen").GetComponent<SpriteRenderer> ();
+				spriteRenderer.sprite = null; 
+				spriteRenderer2 = GameObject.Find ("bad-bubble").GetComponent<SpriteRenderer> ();
+				spriteRenderer2.sprite = null; 
+
 		}
-	}
+	
+		void Update ()
+		{
+				if (end) {
+						counter = counter - 1;
+						if (counter <= 200) {
+								foreach (GameObject g in GameObject.FindGameObjectsWithTag("ground")) {
+										g.AddComponent<Rigidbody2D> ();
+								}
+						}
+						if (counter <= 0) {
+								Application.LoadLevel (Application.loadedLevelName);
+
+						}
+				}
+
+		}
+	
+		void changeSprites ()
+		{
+				if (evil) { 
+						spriteRenderer.sprite = sprite2;
+						spriteRenderer2.sprite = sprite4;
+				} else {
+						spriteRenderer.sprite = sprite1; 
+						spriteRenderer2.sprite = sprite3;
+				}
+
+		end = true;
+		}
+	
+		void OnTriggerEnter2D (Collider2D col)
+		{
+				if (col.gameObject.tag == "Player") {
+						changeSprites ();
+				}
+		}
 	
 }
