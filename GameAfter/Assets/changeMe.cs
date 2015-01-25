1fce82c2956593710642236fa10feeb79ca862eb
Johanna Pirker <joeyprink@gmail.com>
2015-01-25 14:06:05 +0100

    lvl2

---
+++ GameAfter/Assets/changeMe.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class changeMe : MonoBehaviour {
+
+
+	public int speed = 20;
+	public Sprite sprite1;
+	public Sprite sprite2;
+	public Sprite sprite3;
+	SpriteRenderer spriteRenderer;
+	private int counter = 0;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		counter = counter + 1;
+		spriteRenderer = this.GetComponent<SpriteRenderer> ();
+		if(counter%speed == 0) {
+			if(spriteRenderer.sprite == sprite3) {
+				spriteRenderer.sprite = sprite1;
+			} else if(spriteRenderer.sprite == sprite2) {
+				spriteRenderer.sprite = sprite3;
+			}
+			else if(spriteRenderer.sprite == sprite1) {
+				spriteRenderer.sprite = sprite2;
+			}
+		}
+	}
+}
